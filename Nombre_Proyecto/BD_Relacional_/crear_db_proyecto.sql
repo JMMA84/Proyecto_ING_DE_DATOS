a015da6665932dd4a321766f650ddd82010c063e
mauricio-cajica <mauricio.cajica@urosario.edu.co>
2025-03-19 20:54:40 -0500

    correciones y agregados

--- Nombre_Proyecto/BD_Relacional_/crear_db_proyecto.sql
+++ Nombre_Proyecto/BD_Relacional_/crear_db_proyecto.sql
@@ -5,31 +5,51 @@ use bogotaCityFC;
 create table if not exists usuario (
 	nombreUsuario varchar(50) Not null,
 	idUsuario varchar(16) primary key,
+    contrasenia text(50) not null,
 	tipoUsuario varchar(20) not null,
-	telefonoUsuario varchar(10) not null
-);
+	telefonoUsuario varchar(10) not null unique,
+    estadoUsuario bit not null
+ );
+ 
+
 
 create table if not exists rendimientoJugador (
+	idRendimiento varchar(20) primary key,
 	idUsuarioFK varchar(16) not null,
-    idRendimiento varchar(20) primary key,
+    asistencia bit not null,
+    idEventoFK varchar(20) not null,
     metrosRecorridos int not null,
     pasesRealizados int not null,
+    golesRealizados int not null,
     pasesFallidos int not null,
     golesFallidos int not null,
-    
+    comentarios varchar(100) null,
     foreign key (idUsuarioFK) references usuario(idUsuario) on delete cascade
+);
+
+
+create table if not exists categoria(
+	idRendimientoFK varchar(20) primary key,
+    edadMinima tinyint not null,
+    edadMaxima tinyint not null,
+    foreign key (idRendimientoFK) references rendimientoJugador(idRendimiento) on delete cascade
 );
 
 
 create table if not exists evento(
 	idEvento varchar(20) primary key,
+    idRendimientoFK varchar(20) not null,
     fechaEvento date not null,
-    categoria varchar(6), /* ejemplo: sub16F | sub(3 caracteres) | 16 (2 caracteres) | F(1 caracter) | */
-    tipoEvento varchar(50)
+    tipoEvento varchar(50),
+    resumenEvento varchar(100),
+    foreign key (idrendimientoFK) references rendimientoJugador(idRendimiento) on delete cascade
 );
 
-create table if not exists categoria(
-	idCategoria varchar(20) primary key,
-    edadMinima tinyint not null,
-    edadMaxima tinyint not null
-);
+create table if not exists actividadBD(
+	idActividad int primary key auto_increment,
+    registro varchar(100) not null
+)
+
+select * from usuario;
+
+/*	drop schema bogotacityfc;	*/
