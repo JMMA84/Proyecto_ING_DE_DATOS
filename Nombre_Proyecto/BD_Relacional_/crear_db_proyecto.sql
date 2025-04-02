4701f08ca0870152f85dfba15542736f80f7eb05
mauricio-cajica <mauricio.cajica@urosario.edu.co>
2025-04-01 21:12:27 -0500

    mejoras

--- Nombre_Proyecto/BD_Relacional_/crear_db_proyecto.sql
+++ Nombre_Proyecto/BD_Relacional_/crear_db_proyecto.sql
@@ -1,55 +1,70 @@
-create database if not exists bogotaCityFC;
+CREATE DATABASE IF NOT EXISTS bogotaCityFC;
 
-use bogotaCityFC;
+USE bogotaCityFC;
 
-create table if not exists usuario (
-	nombreUsuario varchar(50) Not null,
-	idUsuario varchar(16) primary key,
-    contrasenia text(50) not null,
-	tipoUsuario varchar(20) not null,
-	telefonoUsuario varchar(10) not null unique,
-    estadoUsuario bit not null
- );
- 
-
-
-create table if not exists rendimientoJugador (
-	idRendimiento varchar(20) primary key,
-	idUsuarioFK varchar(16) not null,
-    asistencia bit not null,
-    idEventoFK varchar(20) not null,
-    metrosRecorridos int not null,
-    pasesRealizados int not null,
-    golesRealizados int not null,
-    pasesFallidos int not null,
-    golesFallidos int not null,
-    comentarios varchar(100) null,
-    foreign key (idUsuarioFK) references usuario(idUsuario) on delete cascade
+CREATE TABLE IF NOT EXISTS usuario (
+    nombreUsuario VARCHAR(50) NOT NULL,
+    idUsuario INT PRIMARY KEY auto_increment,
+    contrasenia TEXT(50) NOT NULL,
+    tipoUsuario VARCHAR(20) NOT NULL,
+    telefonoUsuario VARCHAR(10) NOT NULL UNIQUE,
+    estadoUsuario BIT NOT NULL
 );
 
-
-create table if not exists categoria(
-	idRendimientoFK varchar(20) primary key,
-    edadMinima tinyint not null,
-    edadMaxima tinyint not null,
-    foreign key (idRendimientoFK) references rendimientoJugador(idRendimiento) on delete cascade
+CREATE TABLE IF NOT EXISTS categoria (
+    idCategoria int PRIMARY KEY auto_increment,
+    nombreCategoria VARCHAR(50) NOT NULL
 );
 
-
-create table if not exists evento(
-	idEvento varchar(20) primary key,
-    idRendimientoFK varchar(20) not null,
-    fechaEvento date not null,
-    tipoEvento varchar(50),
-    resumenEvento varchar(100),
-    foreign key (idrendimientoFK) references rendimientoJugador(idRendimiento) on delete cascade
+CREATE TABLE IF NOT EXISTS evento (
+    idEvento int PRIMARY KEY auto_increment,
+    fechaEvento DATE NOT NULL,
+    tipoEvento VARCHAR(50),
+    resumenEvento VARCHAR(100)
 );
 
-create table if not exists actividadBD(
-	idActividad int primary key auto_increment,
-    registro varchar(100) not null
-)
+CREATE TABLE IF NOT EXISTS rendimientoJugador (
+    idRendimiento int PRIMARY KEY AUTO_INCREMENT,
+    idUsuarioFK int NOT NULL,
+    idEventoFK int NOT NULL,
+    asistencia BIT NOT NULL,
+	metrosRecorridos INT NOT NULL,
+    pasesRealizados INT NOT NULL,
+    golesRealizados INT NOT NULL,
+    pasesFallidos INT NOT NULL,
+    golesFallidos INT NOT NULL,
+    comentarios VARCHAR(100) NULL,
+    FOREIGN KEY (idUsuarioFK) REFERENCES usuario(idUsuario) ON DELETE CASCADE,
+    FOREIGN KEY (idRendimiento) REFERENCES categoria(idCategoria) ON DELETE CASCADE,
+    FOREIGN KEY (idEventoFK) REFERENCES evento(idEvento) ON DELETE CASCADE
+);
 
-select * from usuario;
+-- DROP TABLE actividadBD;
+CREATE TABLE IF NOT EXISTS actividadBD (
+    idActividad INT PRIMARY KEY AUTO_INCREMENT,
+    registro VARCHAR(100) NOT NULL,
+    fechaActividad DATETIME NOT NULL
+);
 
-/*	drop schema bogotacityfc;	*/
+SELECT * FROM usuario;
+SELECT * FROM categoria;
+SELECT * FROM evento;
+SELECT * FROM rendimientoJugador;
+
+DESCRIBE evento;
+DESCRIBE categoria;
+DESCRIBE rendimientoJugador;
+DESCRIBE usuario;
+
+CALL registrarRendimiento(91,1, 100,  20, 2,  10,  1,'comentario de prueba', 'Juvenil',1);
+CALL registrarRendimiento(90,1, 50,  10, 1,  5,  1,'comentario de prueba', 'Juvenil',1);
+CALL registrarRendimiento(89,1, 200,  20, 3,  20,  1,'comentario de prueba', 'Juvenil',1);
+CALL registrarRendimiento(88,1, 100,  20, 2,  10,  1,'comentario de prueba', 'Juvenil',1);
+CALL registrarRendimiento(87,1, 0,  0, 0,  0,  0,'comentario de prueba', 'Juvenil',1);
+CALL registrarRendimiento(86,1, 1,  1, 1,  1,  1,'comentario de prueba', 'Juvenil',1);
+
+DELETE FROM categoria WHERE idCategoria =12;
+DELETE FROM rendimientoJugador WHERE idRendimiento = 8;
+
+SELECT * FROM actividadBD;
+/* DROP DATABASE bogotaCityFC; */
